38d613e5911c64b347864590f25711fe35c3a7af
Alexandru Ivanescu <ivanescu.alexandru01@gmail.com>
2023-01-05 00:15:36 +0200

    first commit

---
+++ Hide.js
@@ -0,0 +1,20 @@
+
+function hideButton() {
+    const El = document.querySelector(".Content").style.visibility = 'hidden';
+
+    console.log('Paragraph are hide');
+}
+
+
+function visibleButton() {
+    const El = document.querySelector(".Content").style.visibility = 'visible';
+
+    console.log('Paragraph are visible on the screen');
+}
+
+document.getElementById("background").style.color = "orange";
+
+document.getElementById("background").style.backgroundColor = "black";
+
+document.getElementById('body').style.backgroundColor = "mediumseagreen"
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+
+Added an paragraph and two buttons.
+The first button are function to hide text and the secound button are function to take back paragraph(visible text).
+
+Use html , css , bootstrap and javascript.
+
+Enjoy the procces.
